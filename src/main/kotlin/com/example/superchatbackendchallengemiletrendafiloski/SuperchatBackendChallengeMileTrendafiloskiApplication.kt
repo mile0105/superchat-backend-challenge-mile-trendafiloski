65be0833f8ac9f409dba919c3f10f52f142ff703
mile0105 <mt010595@gmail.com>
2022-03-13 21:40:58 +0100

    Init commit

---
+++ src/main/kotlin/com/example/superchatbackendchallengemiletrendafiloski/SuperchatBackendChallengeMileTrendafiloskiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.superchatbackendchallengemiletrendafiloski
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SuperchatBackendChallengeMileTrendafiloskiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SuperchatBackendChallengeMileTrendafiloskiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/superchatbackendchallengemiletrendafiloski/SuperchatBackendChallengeMileTrendafiloskiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.superchatbackendchallengemiletrendafiloski
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SuperchatBackendChallengeMileTrendafiloskiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
